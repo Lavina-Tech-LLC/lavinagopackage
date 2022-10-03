db25b4469d5bc7b23f5ad7d5b2354ac53d7668d1
Mason Siddikov <m.siddikov@gmail.com>
2022-10-03 13:43:26 +0500

    camelCase

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Lavina-Tech-LLC/lavinagopackage
 
 go 1.16
+
+require github.com/graniticio/granitic v1.3.0 // indirect
--- lavina.go
+++ lavina.go
@@ -1,5 +1,10 @@
 package lavina
 
+import (
+	"encoding/json"
+	"strings"
+)
+
 type response struct {
 	Data    interface{} `json:"data"`
 	Message string      `json:"message"`
@@ -12,4 +17,39 @@ func Response(data interface{}, message string, status bool) response {
 	result.Data = data
 	result.IsOk = status
 	return result
+}
+
+func ResponseCamelCase(data interface{}, message string, status bool) (string, []byte) {
+	result := response{}
+	result.Message = message
+	result.Data = data
+	result.IsOk = status
+	bytes, _ := json.Marshal(result)
+
+	return "application/json", convertKeys(json.RawMessage(bytes))
+}
+
+func convertKeys(j json.RawMessage) json.RawMessage {
+	m := make(map[string]json.RawMessage)
+	if err := json.Unmarshal([]byte(j), &m); err != nil {
+		// Not a JSON object
+		return j
+	}
+
+	for k, v := range m {
+		fixed := fixKey(k)
+		delete(m, k)
+		m[fixed] = convertKeys(v)
+	}
+
+	b, err := json.Marshal(m)
+	if err != nil {
+		return j
+	}
+
+	return json.RawMessage(b)
+}
+
+func fixKey(key string) string {
+	return strings.ToUpper(key)
 }
