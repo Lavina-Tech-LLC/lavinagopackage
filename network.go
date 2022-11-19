bb853e16e3f01b2977a07f4241b4fddc678cf6cd
Mason Siddikov <m.siddikov@gmail.com>
2022-11-19 19:13:27 +0500

    GinErr

--- network.go
+++ network.go
@@ -1,5 +1,7 @@
 package lvn
 
+import "github.com/gin-gonic/gin"
+
 type response struct {
 	Data    interface{} `json:"data"`
 	Message string      `json:"message"`
@@ -17,6 +19,14 @@ func Response(data interface{}, message string, status bool) response {
 // Response for using with *gin.Context.Data() body is forced as camelCase
 func Res(statusCode int, data interface{}, message string, omitKeys ...string) (int, string, []byte) {
 	return res(statusCode, data, message, omitKeys, []string{})
+}
+
+// Receives gin.Context and error, and if err!=nil aborts context with error
+func GinErr(c *gin.Context, statusCode int, err error, message string) {
+	if err != nil {
+		c.Data(Res(statusCode, err.Error(), message))
+		c.Abort()
+	}
 }
 
 func ResSelected(statusCode int, data interface{}, message string, selectKeys ...string) (int, string, []byte) {
